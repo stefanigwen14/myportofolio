bdd429bc78c492130ee56e552fad4b18cff8397b
Stefani Gwen Rolanda Tumbelaka <stefani.gwen@ui.ac.id>
2026-09-13 10:31:36 +0700

    feat: Menambahkan page (models) Education

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from main.models import Experience
+from main.models import Education
 
 
 def show_main(request):
@@ -23,3 +24,11 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Stefani Gwen Rolanda Tumbelaka",
+        "nickname": "Gwen",
+        "education_list": Education.objects.all(),
+    }
+    return render(request, "education.html", context)
